0728d94aac0edb129d23558c6f05e57b3eb5a38a
Niclas <niclastimmdev@gmail.com>
2023-11-30 12:00:52 +0100

    Update loc command arguments and options

--- src/loc.js
+++ src/loc.js
@@ -32,10 +32,91 @@ const getFiles = (dirPath, options) => {
         ? options.excludeDirs.split(',').map(dir => `${path.join(dirPath, dir)}/**`)
         : [];
 
+    if (options.useDefaultIgnoreDirs) {
+        ignoreDirs.push(...defaultIgnoreDirs(dirPath));
+    }
+
     const ignoreExtensions = options.excludeExtensions
         ? options.excludeExtensions.split(',').map(ext => `**/*.${ext}`)
         : [];
 
+    if (options.useDefaultIgnoreExt) {
+        ignoreExtensions.push(...defaultIgnoreExts());
+    }
+
     return glob.sync(`${dirPath}/**/*.*`, { ignore: [...ignoreDirs, ...ignoreExtensions] });
 }
+
+function defaultIgnoreExts() {
+    const exts = [
+        'woff',
+        'woff2',
+        'eot',
+        'ttf',
+        'otf',
+        'png',
+        'jpg',
+        'jpeg',
+        'gif',
+        'svg',
+        'ico',
+        'mp4',
+        'mp3',
+        'wav',
+        'ogg',
+        'avi',
+        'mov',
+        'webm',
+        'mkv',
+        'zip',
+        'tar',
+        'gz',
+        '7z',
+        'rar',
+        'pdf',
+        'doc',
+        'docx',
+        'xls',
+        'xlsx',
+        'ppt',
+        'pptx',
+        'csv'
+    ];
+
+    return exts.map(ext => `**/*.${ext}`);
+}
+
+function defaultIgnoreDirs(dirPath) {
+    const dirs = [
+        'vendor',
+        'node_modules',
+        'bower_components',
+        'public',
+        'dist',
+        'build',
+        'tmp',
+        'temp',
+        'out',
+        'output',
+        'logs',
+        'flow-typed',
+        'coverage',
+        'android',
+        'ios',
+        'img',
+        'images',
+        'assets',
+        'fonts',
+        'videos',
+        'audio',
+        'docs',
+        'lang',
+        'locales',
+        'translations',
+        'data'
+    ];
+
+    return dirs.map(dir => `${path.join(dirPath, dir)}/**`);
+}
+
 module.exports = loc;
